7f8c55d8a7a2a4bddb151655f9a61cb8c7a2a438
vagrant <vagrant@vagrant.vm>
2020-04-10 07:54:52 +0000

    rateがわかるように対応、入出力時のエラーハンドリングを整備する必要がある

--- parse_summoner/src/main.rs
+++ parse_summoner/src/main.rs
@@ -1,7 +1,9 @@
 extern crate url;
 extern crate reqwest;
 
+//use std::env;
 use std::env;
+// use std::path::Path;
 use std::fs::File;
 use std::io::BufReader;
 use std::collections::BTreeMap;
@@ -53,6 +55,61 @@ impl Summoner_v4 {
 
         let resp: Self = reqwest::blocking::get(&url)?.json()?;
 
+        Ok(resp)
+    }
+}
+
+
+
+
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+struct League_v4 {
+    leagueId: String,
+    queueType: String,
+    tier: String,
+    rank: String,
+    summonerId: String,
+    summonerName: String,
+    leaguePoints: i32,
+    wins: i32,
+    losses: i32,
+    veteran: bool,
+    inactive: bool,
+    freshBlood: bool,
+    hotStreak: bool
+}
+
+impl Default for League_v4 {
+    fn default() -> Self {
+        Self {
+            leagueId: String::default(),
+            queueType: String::default(),
+            tier: String::default(),
+            rank: String::default(),
+            summonerId: String::default(),
+            summonerName: String::default(),
+            leaguePoints: i32::default(),
+            wins: i32::default(),
+            losses: i32::default(),
+            veteran: bool::default(),
+            inactive: bool::default(),
+            freshBlood: bool::default(),
+            hotStreak: bool::default()
+        }
+    }
+}
+
+impl League_v4 {
+    fn fetch_league_v4(region: &str, id: &str, api_key: &str) -> Result<Vec<Self>, Error> {
+        let url = format!("https://{}1.api.riotgames.com/lol/league/v4/entries/by-summoner/{}?api_key={}",
+            region,
+            id,
+            api_key
+        );
+    
+        let resp: Vec<Self> = reqwest::blocking::get(&url)?.json()?;
         Ok(resp)
     }
 }
@@ -121,7 +178,7 @@ fn get_champ_id_map() -> BTreeMap<i64, String> {
 
 
 
-fn resp_lol_data(sn: &str) -> Result<Vec<String>, Error> {
+fn resp_mastery(sn: &str) -> Result<Vec<String>, Error> {
 
     // to use env
     dotenv().ok();
@@ -131,7 +188,6 @@ fn resp_lol_data(sn: &str) -> Result<Vec<String>, Error> {
     let champ_id_map = get_champ_id_map();
    
     // let name = sn;
-    println!("SN = {}", sn);
     let name = sn;
     let region = "jp";
 
@@ -140,19 +196,18 @@ fn resp_lol_data(sn: &str) -> Result<Vec<String>, Error> {
 
     let champion_mastery_v4_resp = Champion_mastery_v4::fetch_champion_mastery_v4(region, &summoner_v4_resp.id, api_key)?;
 
-
     let loop_num: usize;
-    if champion_mastery_v4_resp.len() < 7 {
+    if champion_mastery_v4_resp.len() < 5 {
         loop_num = champion_mastery_v4_resp.len();
     } else {
-        loop_num = 7;
+        loop_num = 5;
     }
 
 
     let mut data_vec = Vec::new();
     for i in 0..loop_num {
-        let data_str = format!("{: >4}) {: <9} {: >7} ({})",
-            i+1,
+        let data_str = format!("{: <4} {: <12} {: <8} ({})",
+            (i+1).to_string() +")",
             champ_id_map.get(&champion_mastery_v4_resp[i].championId).unwrap(),
             champion_mastery_v4_resp[i].championPoints,
             champion_mastery_v4_resp[i].championLevel
@@ -165,9 +220,53 @@ fn resp_lol_data(sn: &str) -> Result<Vec<String>, Error> {
 
 
 
+fn resp_league(sn: &str) -> Result<Vec<String>, Error> {
+
+    // to use env
+    dotenv().ok();
+
+    // Check Riot API
+    let api_key = &env::var("RIOT_API_KEY").expect("Please setting RIOT_API_KEY");
+   
+    // let name = sn;
+    let name = sn;
+    let region = "jp";
+
+
+    let summoner_v4_resp = Summoner_v4::fetch_summoner_v4(region, name, api_key)?;
+    let league_v4_resp_vec = League_v4::fetch_league_v4(region, &summoner_v4_resp.id, api_key)?;
+
+
+    let mut data_vec = Vec::new();
+    data_vec.push(
+        format!("{}    {}  {}    {}    {}",
+            "Queue", "Tier", "Division", "LP", "Win Rate"
+        )    
+    );
+
+    for league_v4_resp in league_v4_resp_vec {
+        let win_rate: i32 = league_v4_resp.wins*100 / (league_v4_resp.wins+league_v4_resp.losses);
+        let data_str = format!("{}    {}  {}    {}LP    {}%",
+            league_v4_resp.queueType,
+            league_v4_resp.tier,
+            league_v4_resp.rank,
+            league_v4_resp.leaguePoints,
+            win_rate
+        );
+        data_vec.push(data_str);
+    }
+
+    Ok(data_vec)
+}
+
+
+
+
+
 use serenity::{
     model::{channel::Message, gateway::Ready},
     prelude::*,
+    //http::AttachmentType,
 };
 
 struct Handler;
@@ -178,29 +277,68 @@ impl EventHandler for Handler {
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!") {
                 println!("Error sending message: {:?}", why);
             }
-        } else if msg.content.starts_with("!mastary") {
+        } else if msg.content.starts_with("!mastery") {
             let sn: Vec<&str> = msg.content.split_whitespace().collect();
-            match sn.len() {
-                2 => {
-                    match resp_lol_data(sn[1]) {
-                        Ok(data_vec) => { 
-                            for data in data_vec {
-                                if let Err(why) = msg.channel_id.say(&ctx.http, data) {
-                                    println!("Error sending message: {:?}", why);
-                                }
-                            }
-                        },
-                        Err(err) => {
-                            println!("{}", err);
+            if sn.len() >= 2 {
+                match resp_mastery(sn[1]) {
+                    Ok(data_vec) => {
+                        let data = data_vec.join("\n");
+                        if let Err(why) = msg.channel_id.say(&ctx.http, data) {
+                            println!("Error sending message: {:?}", why);
                         }
-                    };
-                }
-                _ => {
-                    if let Err(why) = msg.channel_id.say(&ctx.http, "!mastary SN を入力して下さい") {
-                        println!("Error sending message: {:?}", why);
+                    },
+                    Err(err) => {
+                        println!("{}", err);
                     }
                 }
-            } 
+            } else {
+                if let Err(why) = msg.channel_id.say(&ctx.http, "!mastery SN を入力して下さい") {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+        } else if  msg.content.starts_with("!rate") {
+            let sn: Vec<&str> = msg.content.split_whitespace().collect();
+            if sn.len() >= 2 {
+                match resp_league(sn[1]) {
+                    Ok(data_vec) => {
+                        let data = data_vec.join("\n");
+                        if let Err(why) = msg.channel_id.say(&ctx.http, data) {
+                            println!("Error sending message: {:?}", why);
+                        }
+                    },
+                    Err(err) => {
+                        println!("{}", err);
+                    }
+                }
+            } else {
+                if let Err(why) = msg.channel_id.say(&ctx.http, "!mastery SN を入力して下さい") {
+                    println!("Error sending message: {:?}", why);
+                }
+            }
+        } else if msg.content == "!hello" {
+            let msg = msg.channel_id.send_message(&ctx.http, |m| {
+                m.content("Hello, World!");
+                m.embed(|e| {
+                    e.title("Mastary");
+                    //e.description("");
+                    // e.image("attachment://ferris_eyes.png");a
+                    let data_vec: Vec<&str> = vec!["str1", "str2", "str3"];
+                    let data = data_vec.join("\n");
+                    e.field("a", data, true);
+                    
+                    e
+                });
+                // m.add_file(AttachmentType::Path(Path::new("./ferris_eyes.png")));
+                m
+            });
+
+            if let Err(why) = msg {
+                println!("Error sending message: {:?}", why);
+            }
+        } else if msg.content == "!さかとし" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "愛してる") {
+                println!("Error sending message: {:?}", why);
+            }
         }
     }
 
@@ -209,14 +347,10 @@ impl EventHandler for Handler {
     }
 }
 
-
-
 fn main() {
-
-    // Check Discord Token    
+    // Configure the client with your Discord bot token in the environment.
     let token = env::var("DISCORD_TOKEN")
         .expect("Expected a token in the environment");
-
     let mut client = Client::new(&token, Handler).expect("Err creating client");
 
     if let Err(why) = client.start() {
